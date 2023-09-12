956d4a796eae64ecf84b424a8f017e60c0f98156
Duologic <jeroen@simplistic.be>
2023-09-12 14:13:07 +0200

    remove reference highlighting, too much impact on performance

--- queries/jsonnet/highlights.scm
+++ queries/jsonnet/highlights.scm
@@ -4,37 +4,37 @@
 
 ;;; References - depends on locals.scm
 
-; Make reference same color as parameter 
-; (may incur performance issues on big files)
-; Depends on locals.scm
-((id) @parameter.reference
- (#is? @parameter.reference parameter))
-
-((id) @function.reference
- (#is? @function.reference function))
-
-((id) @variable.reference
- (#is? @variable.reference var))
-
-((id) @variable.local
- (#is? @variable.reference var))
-
-; References do not apply to static field IDs
-; Workaround for `(#is-not? local)` not being supported
-(fieldname (id) @field)
-(fieldname (string (string_content) @field))
-
-; But it does apply if ID in an expression
-(fieldname
- ("["
-  (id) @parameter.reference
-  "]"
-  (#is? @parameter.reference parameter)))
-(fieldname
- ("["
-  (id) @variable.local
-  "]"
-  (#is? @variable.reference var)))
+;; Make reference same color as parameter
+;; (may incur performance issues on big files)
+;; Depends on locals.scm
+;((id) @parameter.reference
+; (#is? @parameter.reference parameter))
+;
+;((id) @function.reference
+; (#is? @function.reference function))
+;
+;((id) @variable.reference
+; (#is? @variable.reference var))
+;
+;((id) @variable.local
+; (#is? @variable.reference var))
+;
+;; References do not apply to static field IDs
+;; Workaround for `(#is-not? local)` not being supported
+;(fieldname (id) @field)
+;(fieldname (string (string_content) @field))
+;
+;; But it does apply if ID in an expression
+;(fieldname
+; ("["
+;  (id) @parameter.reference
+;  "]"
+;  (#is? @parameter.reference parameter)))
+;(fieldname
+; ("["
+;  (id) @variable.local
+;  "]"
+;  (#is? @variable.reference var)))
 
 ;;; Linting
 
